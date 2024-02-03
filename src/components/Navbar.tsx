37c968602e3582b7e4efe00e66571e534c27cc3a
Aryan Bhosale <aryan.bhosale1719@gmail.com>
2024-02-03 19:13:01 +0530

    loginstate

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,42 +1,66 @@
-import Link from "next/link"
-import MaxWidthWrapper from "./MaxWidthWrapper"
-import { buttonVariants } from "./ui/button"
-import { LoginLink, RegisterLink } from "@kinde-oss/kinde-auth-nextjs/components"
-import { ArrowRight } from "lucide-react"
+import Link from "next/link";
+import MaxWidthWrapper from "./MaxWidthWrapper";
+import { buttonVariants } from "./ui/button";
+import {
+  LoginLink,
+  RegisterLink,
+} from "@kinde-oss/kinde-auth-nextjs/components";
+import { ArrowRight } from "lucide-react";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
-const Navbar = () => {
+const Navbar = async () => {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
   return (
     <nav className="sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-700 bg-black/75 backdrop-blur-sm transition-all rounded-2xl">
-        <MaxWidthWrapper>
-            <div className="flex h-14 items-center justify-between border-b border-zinc-900">
-                <Link href="/" className="flex z-48 font-semibold text-2xl">
-                    parchment.
-                </Link>
+      <MaxWidthWrapper>
+        <div className="flex h-14 items-center justify-between border-b border-zinc-900">
+          <Link href="/" className="flex z-48 font-semibold text-2xl">
+            parchment.
+          </Link>
 
-
-                <div className="hidden items-center space-x-4 sm:flex">
-                    <>
-                        <Link href="/pricing" className={buttonVariants({
-                            variant: "ghost",
-                            size: "sm"
-                        })}>Pricing</Link>
-                        <LoginLink className={buttonVariants({
-                            variant: "ghost",
-                            size: "sm"
-                        })}>
-                            Sign in
-                        </LoginLink>
-                        <RegisterLink className={buttonVariants({
-                            size: "sm"
-                        })}>
-                            Dive in <ArrowRight className='ml-1.5 h-5 w-5' />
-                        </RegisterLink>
-                    </>
+          <div className="hidden items-center space-x-4 sm:flex">
+            <>
+              <Link
+                href="/pricing"
+                className={buttonVariants({
+                  variant: "ghost",
+                  size: "sm",
+                })}
+              >
+                Pricing
+              </Link>
+              {!user ? (
+                <>
+                  <LoginLink
+                    className={buttonVariants({
+                      variant: "ghost",
+                      size: "sm",
+                    })}
+                  >
+                    Sign in
+                  </LoginLink>
+                  <RegisterLink
+                    className={buttonVariants({
+                      size: "sm",
+                    })}
+                  >
+                    Dive in <ArrowRight className="ml-1.5 h-5 w-5" />
+                  </RegisterLink>
+                </>
+              ) : (
+                <div className="mx-auto cursor-pointer flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/80">
+                  <p className="text-sm font-semibold text-gray-700">
+                    {user?.given_name} {user?.family_name}
+                  </p>
                 </div>
-            </div>
-        </MaxWidthWrapper>
+              )}
+            </>
+          </div>
+        </div>
+      </MaxWidthWrapper>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
