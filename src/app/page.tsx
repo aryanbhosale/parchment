8cc946f14a9afcfa7d7ac833b655c1003d646689
Aryan Bhosale <aryan.bhosale1719@gmail.com>
2024-02-24 19:12:04 +0530

    space+deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link'
 import Image from 'next/image'
 
 export default function Home() {
+  
   return (
     <>
       <MaxWidthWrapper className='mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center'>
